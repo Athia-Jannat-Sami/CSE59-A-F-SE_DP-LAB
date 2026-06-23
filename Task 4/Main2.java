15727d6ccc7cc605866bdbe6579979205961b85b
Athia-Jannat-Sami <athiajannat18@gmail.com>
2026-06-23 10:38:50 +0600

    Update Main2.java

--- Task 4/Main2.java
+++ Task 4/Main2.java
@@ -1,1 +1,51 @@
+class TeamMember {
+    protected String name;
 
+    public TeamMember(String name) {
+        this.name = name;
+    }
+
+    public void showName() {
+        System.out.println("Name: " + name);
+    }
+}
+
+
+interface BonusEligible {
+    void calculateBonus();
+}
+
+
+class Employee extends TeamMember implements BonusEligible {
+
+    public Employee(String name) {
+        super(name);
+    }
+
+    public void calculateBonus() {
+        System.out.println("Calculating standard employee bonus...");
+    }
+}
+
+
+class Contractor extends TeamMember {
+
+    public Contractor(String name) {
+        super(name);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Employee employee = new Employee("John");
+        employee.showName();
+        employee.calculateBonus();
+
+
+        Contractor contractor = new Contractor("Mike");
+        contractor.showName();
+    }
+}
