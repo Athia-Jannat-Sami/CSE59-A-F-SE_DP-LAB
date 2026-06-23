6842c7cd4c1a1408b07ded172eb7776fb7302790
Athia-Jannat-Sami <athiajannat18@gmail.com>
2026-06-23 11:30:53 +0600

    Update Main4.java

--- Task 4/Main4.java
+++ Task 4/Main4.java
@@ -1,1 +1,59 @@
+interface MessageSender {
+    void sendMessage(String message);
+}
+
+
+class EmailSender implements MessageSender {
+    
+    public void sendMessage(String message) {
+        System.out.println("Sending email: " + message);
+    }
+}
+
+
+class SMSSender implements MessageSender {
+
+    public void sendMessage(String message) {
+        System.out.println("Sending SMS: " + message);
+    }
+}
+
+
+
+class NotificationService {
+
+    private MessageSender sender;
+
+
+    public NotificationService(MessageSender sender) {
+        this.sender = sender;
+    }
+
+
+    public void alertUser(String msg) {
+        sender.sendMessage(msg);
+    }
+}
+
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        MessageSender email = new EmailSender();
+
+        NotificationService notification =
+                new NotificationService(email);
+
+        notification.alertUser("Hello User");
+
+
+        MessageSender sms = new SMSSender();
+
+        NotificationService notification2 =
+                new NotificationService(sms);
+
+        notification2.alertUser("Hello User");
+    }
+}
 
