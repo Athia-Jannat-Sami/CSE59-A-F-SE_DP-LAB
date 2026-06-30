c85b48ff10102dc3675b17ea15637e9aee86b42f
Athia-Jannat-Sami <athiajannat18@gmail.com>
2026-06-30 10:39:54 +0600

    Update Main2.java

--- Task 5/Main2.java
+++ Task 5/Main2.java
@@ -1,1 +1,35 @@
+interface MessageSender {
+       void sendMessage(String message);
+}
+
+class EmailSender implements MessageSender {
+    public void sendMessage(String message) {
+        System.out.println("Sending Email: " + message);
+    }
+}
+
+class NotificationService {
+
+    private MessageSender messageSender;
+
+    public NotificationService (MessageSender messageSender){
+
+        this.messageSender = messageSender;
+    }
+
+    public void alertUser(String msg){
+        messageSender.sendMessage(msg);
+    }
+
+}
+
+
+public class Main2 {
+    public static void main(String[] args) {
+        MessageSender sender = new EmailSender();
+        NotificationService notification = new NotificationService(sender);
+        notification.alertUser("Hello! This is a notification.");
+        
+    }
+}
 
