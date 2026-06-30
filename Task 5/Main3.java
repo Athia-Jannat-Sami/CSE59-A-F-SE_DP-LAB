507697618ee5c64251e83ab72a7d1aaffb611095
Athia-Jannat-Sami <athiajannat18@gmail.com>
2026-06-30 11:12:38 +0600

    Update Main3.java

--- Task 5/Main3.java
+++ Task 5/Main3.java
@@ -1,1 +1,36 @@
+class Patient{
+    private String patientName;
+    private String nationalId;
+
+    public Patient(String patientName, String nationalId){
+        this.patientName = patientName;
+        this.nationalId = nationalId;
+    }
+
+    public String getPatientName(){
+        return patientName;
+    }
+
+    public String getNationalId(){
+        return nationalId;
+    }
+
+}
+
+class IdValidator {
+    public boolean validateId(String nationalId){
+        return nationalId.length() == 10 || nationalId.length() == 17;
+    }
+}
+
+class SmsService {
+    public void sendSmsConfirmation(Patient patient){
+        IdValidator validator = new IdValidator();
+        if(validator.validateId(patient.getNationalId())){
+            System.out.println("Sending sms to " + patient.getPatientName() + ": Registration successful.");
+        }else{
+            System.out.println("Invalid National Id.");
+        }
+    }
+}
 
